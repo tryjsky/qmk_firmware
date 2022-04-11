8139c0a1b23b170a84dc9ad3861ccf3879ea7586
Y., Ryota <tryjsky@gmail.com>
2022-04-12 00:47:46 +0900

    Initial release

---
+++ keyboards/handwired/keypad12/keypad12.h
@@ -0,0 +1,28 @@
+// Copyright 2022 Y., Ryota (@tryjsky)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "quantum.h"
+
+/* This is a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+#define LAYOUT( \
+    k00, k01, k02, k03, \
+    k10, k11, k12, k13, \
+    k20, k21, k22, k23, \
+    k30, k31, k32, k33, \
+    k40, k41, k42 \
+) { \
+    { k00, k01, k02, k03   }, \
+    { k10, k11, k12, k13   }, \
+    { k20, k21, k22, k23   }, \
+    { k30, k31, k32, k33   }, \
+    { k40, k41, k42, KC_NO }  \
+}
