ded769d5ddd33c101cafdf40eca3c309ca66854d
Y., Ryota <tryjsky@gmail.com>
2022-05-13 01:10:17 +0900

    Add bootmagic and LED pin

--- keyboards/dreamcast/config.h
+++ keyboards/dreamcast/config.h
@@ -6,6 +6,13 @@
 #define USB_MAX_POWER_CONSUMPTION 100
 #define MATRIX_HAS_GHOST
 
+#define BOOTMAGIC_LITE_ROW 12
+#define BOOTMAGIC_LITE_COLUMN 7
+
+#define LED_CAPS_LOCK_PIN D5
+#define LED_SCROLL_LOCK_PIN B0
+#define LED_PIN_ON_STATE 0
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
